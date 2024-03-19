a52770d3da3aff46f0972aec72fb4f7ffad5bd34
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-03-19 10:55:17 +0100

    update grub

--- nixos/system/grub-bootloader.nix
+++ nixos/system/grub-bootloader.nix
@@ -5,6 +5,6 @@
     boot.loader.efi.canTouchEfiVariables = true;
     boot.loader.grub.enable = true;
     boot.loader.grub.efiSupport = true;
-    #boot.loader.grub.useOSProber = true;
+    boot.loader.grub.useOSProber = true;
     boot.loader.grub.configurationLimit = 5;
 }
