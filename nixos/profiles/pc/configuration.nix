bbac59907258e194085635ba3c4f93abb5bece93
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-03-19 07:56:33 +0100

    update plasma5

--- nixos/profiles/pc/configuration.nix
+++ nixos/profiles/pc/configuration.nix
@@ -8,8 +8,8 @@
         ./hardware-configuration.nix
         ../../system/amdgpu.nix
         ../../system/mountsamba.nix
-        #../../system/wm/plasma5.nix
-        ../../system/wm/plasma6.nix
+        ../../system/wm/plasma5.nix
+        #../../system/wm/plasma6.nix
         #../../system/wm/gnome.nix
         #../../system/wm/hyprland.nix
     ];
