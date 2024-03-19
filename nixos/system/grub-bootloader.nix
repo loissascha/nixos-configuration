c41bad212c00a697c856d75841d8ff5eab5a7dfd
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-03-19 10:15:09 +0100

    update grub

--- nixos/system/grub-bootloader.nix
+++ nixos/system/grub-bootloader.nix
@@ -3,7 +3,7 @@
 {
     #Grub Bootloader.
     boot.loader.grub.enable = true;
-    boot.loader.grub.device = "/dev/vda";
+    #boot.loader.grub.device = "/dev/vda";
     boot.loader.grub.useOSProber = true;
     boot.loader.grub.configurationLimit = 5;
 }
