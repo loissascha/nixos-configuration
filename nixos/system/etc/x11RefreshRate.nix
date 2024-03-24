e62cbcf95cd13fe2cadd94702ee99ca8fff1da34
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-03-24 12:14:12 +0100

    added x11 refresh rate fix

---
+++ nixos/system/etc/x11RefreshRate.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, ... }:
+
+{
+  environment.etc = {
+    environment = {
+        text = ''
+            CLUTTER_DEFAULT_FPS=240
+            __GL_SYNC_DISPLAY_DEVICE=DP-3
+            __GL_SYNC_TO_VBLANK=0
+        '';
+        mode = "0440";
+    };
+  }
+}
