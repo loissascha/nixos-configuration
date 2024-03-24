3db2842cf9403e09804338ee193a6f30ee5f3e86
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-03-24 13:02:12 +0100

    update steam and gamescope

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -197,7 +197,6 @@
     steam-run
     steamtinkerlaunch
     discord
-    gamescope
     lutris
     mangohud
     goverlay
@@ -256,17 +255,12 @@
   };
 
   
-
+  programs.gamescope.enable = true;
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     gamescopeSession.enable = true;
-    package = pkgs.steam.override {
-      extraPkgs = pkgs: with pkgs; [
-        gamescope
-      ];
-    };
   };
 
   programs.java.enable = true;
