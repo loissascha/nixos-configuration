07ea97b5c6f79ef4a80694c02533b321e176d31e
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-04-04 21:27:00 +0200

    update

--- nixos/profiles/pc/configuration.nix
+++ nixos/profiles/pc/configuration.nix
@@ -12,7 +12,7 @@
         #../../system/wm/plasma5.nix
         ../../system/wm/plasma6.nix
         #../../system/wm/gnome.nix
-        ../../system/wm/hyprland.nix
+        #../../system/wm/hyprland.nix
     ];
 
     boot.loader.grub.devices = [ "nodev" ];
