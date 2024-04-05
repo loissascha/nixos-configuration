daf6cf75d91ebf3cbd4db54e4ffbb87a6d8d6158
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-04-05 12:37:43 +0200

    switch to gnome

--- nixos/profiles/pc/configuration.nix
+++ nixos/profiles/pc/configuration.nix
@@ -10,8 +10,8 @@
         ../../system/amdgpu.nix
         ../../system/mountsamba.nix
         #../../system/wm/plasma5.nix
-        ../../system/wm/plasma6.nix
-        #../../system/wm/gnome.nix
+        #../../system/wm/plasma6.nix
+        ../../system/wm/gnome.nix
         #../../system/wm/hyprland.nix
     ];
 
