eea763eea9fe501b3c4bde7a63c4daa8c63cf55f
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-04-21 21:02:01 +0200

    home update

--- nixos/home.nix
+++ nixos/home.nix
@@ -61,25 +61,5 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  gtk = {
-    enable = true;
-    theme = {
-      name = "Catppuccin-Mocha-Lavender";
-      package = pkgs.catppuccin-gtk.override {
-        accents = [ "lavender" ];
-        size = "compact";
-        tweaks = [ "normal" ];
-        variant = "mocha";
-      };
-    };
-  };
-
-  # Now symlink the `~/.config/gtk-4.0/` folder declaratively:
-  xdg.configFile = {
-    "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
-    "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
-    "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
-  };
-
 
 }
