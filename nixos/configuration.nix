b29ecd75154cb331d12fa5c31529bc69ca73a089
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-05-02 09:12:39 +0200

    nordvpn

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, chaotic, ... }:
 
 {
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
@@ -233,8 +233,6 @@
     vial #qmk keyboard stuff helper
     nordvpn
   ];
-
-  chaotic.nordvpn.enable = true;
 
   environment.shells = with pkgs; [ zsh ];
 
