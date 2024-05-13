27d98241cddc281263378df530db922de7e46297
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-05-13 14:36:44 +0200

    vm update

--- nixos/profiles/vm/configuration.nix
+++ nixos/profiles/vm/configuration.nix
@@ -7,18 +7,18 @@
         ./hostname.nix
         ./hardware-configuration.nix
         ../../system/mountsamba.nix
-        ../../system/dm/sddm.nix
-        ../../system/wm/plasma6.nix
+        #../../system/dm/sddm.nix
+        #../../system/wm/plasma6.nix
     ];
 
-    #services.desktopManager.cosmic.enable = true;
-    #services.displayManager.cosmic-greeter.enable = true;
+    services.desktopManager.cosmic.enable = true;
+    services.displayManager.cosmic-greeter.enable = true;
 
     boot.kernelPackages = pkgs.linuxPackages_zen; 
 
     boot.loader.grub.device = "/dev/vda";
 
-    environment.systemPackages = with pkgs; [
-        protonup-qt
-    ];
+    #environment.systemPackages = with pkgs; [
+    #    protonup-qt
+    #];
 }
