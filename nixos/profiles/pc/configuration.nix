066259211402717885d6cd1d00ce3db68db6443b
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-05-14 16:35:00 +0200

    switch to plasma 5 because of issues with plasma 6

--- nixos/profiles/pc/configuration.nix
+++ nixos/profiles/pc/configuration.nix
@@ -14,14 +14,14 @@
         ../../system/mountsamba.nix
         #../../system/dm/gdm.nix
         ../../system/dm/sddm.nix
-        #../../system/wm/plasma5.nix
-        ../../system/wm/plasma6.nix
+        ../../system/wm/plasma5.nix
+        #../../system/wm/plasma6.nix
         #../../system/wm/gnome.nix
         ../../system/wm/hyprland.nix
     ];
 
-    #boot.kernelPackages = pkgs.linuxPackages_cachyos;
-    boot.kernelPackages = pkgs.linuxPackages_zen; 
+    boot.kernelPackages = pkgs.linuxPackages_cachyos;
+    #boot.kernelPackages = pkgs.linuxPackages_zen; 
 
     boot.loader.grub.devices = [ "nodev" ];
 
